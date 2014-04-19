fcde3ac34eaa939d55f7d9e0638cd22acc59545a
Jason Lamey <jasonlamey@gmail.com>
2014-04-18 21:31:25 -0400

    (image_thumbs) More cleanup, beginnings of Image utils.

---
+++ lib/Side7/Utils/Image.pm
@@ -0,0 +1,104 @@
+package My::Package;
+
+use strict;
+use warnings;
+
+use base 'Side7::DB::Object'; # Only needed if this is a database object.
+
+=pod
+
+=head1 NAME
+
+My::Package
+
+=head1 DESCRIPTION
+
+TODO: Define a package description.
+
+=head1 SCHEMA INFORMATION
+
+    Table name: stages
+     
+    | id         | int(1) unsigned | NO   | PRI | NULL    | auto_increment |
+    | stage      | varchar(45)     | NO   |     | NULL    |                |
+    | priority   | int(1)          | NO   | MUL | NULL    |                |
+    | created_at | datetime        | NO   |     | NULL    |                |
+    | updated_at | datetime        | NO   |     | NULL    |                |
+
+=head1 RELATIONSHIPS
+
+=over
+
+=item Class::Name
+
+TODO: Define the relationship type, and list the foreign key (FK).
+
+=back
+
+=cut
+
+# TODO: Define the appropriate package meta config for the DB Object.
+
+__PACKAGE__->meta->setup
+(
+    table   => 'users',
+    columns => [ 
+        id            => { type => 'integer', not_null => 1 },
+        username      => { type => 'varchar', length => 45,  not_null => 1 }, 
+        email_address => { type => 'varchar', length => 255, not_null => 1 }, 
+        password      => { type => 'varchar', length => 45,  not_null => 1 }, 
+        created_at    => { type => 'datetime', not_null => 1, default => 'now()' }, 
+        updated_at    => { type => 'datetime', not_null => 1, default => 'now()' },
+    ],
+    pk_columns => 'id',
+    unique_key => [ 'username', 'email_address' ],
+    relationships =>
+    [
+        account =>
+        {
+            type       => 'one to one',
+            class      => 'Side7::Account',
+            column_map => { id => 'user_id' },
+        },
+    ],
+);
+
+=head1 METHODS
+
+
+=head2 method_name()
+
+    my $result = My::Package->method_name();
+
+TODO: Define what this method does, describing both input and output values and types.
+
+=cut
+
+sub method_name
+{
+}
+
+
+=head1 FUNCTIONS
+
+
+=head2 function_name()
+
+    my $result = My::Package::function_name();
+
+TODO: Define what this method does, describing both input and output values and types.
+
+=cut
+
+sub function_name
+{
+}
+
+
+=head1 COPYRIGHT
+
+All code is Copyright (C) Side 7 1992 - 2014
+
+=cut
+
+1;
